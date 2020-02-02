3388eda5cbcf9bfdac218c585f64401909c3c99b
thangzjm99 <60538008+thangzjm99@users.noreply.github.com>
2020-02-02 15:38:53 +0700

    Đẩy code
    No one

---
+++ T-SQL/CaoMinhDuc_T_SQL.sql
@@ -0,0 +1,146 @@
+﻿use bai_tap;
+go
+
+--1.them cot trang thai trong bang mon an
+alter table MONAN add trangthai int;
+go
+
+--2.xoa nhan vien co ma nhan vien > 34
+delete NHANVIEN where MANHANVIEN > 34;
+go
+
+--3.bien cuc bo 
+DECLARE @KhachHangId int;
+SET @KhachHangId = 10;
+
+SELECT *
+FROM   KHACHHANG
+WHERE  MAKHACHHANG > @KhachHangId;
+
+--4.cap nhat ten ban thanh ten nhan vien neu co id ban trung voi id nhan vien
+UPDATE BANAN 
+SET LOAIBAN = (SELECT TENNHANVIEN
+          FROM NHANVIEN
+          WHERE NHANVIEN.MANHANVIEN = BANAN.ID_MANHANVIEN)
+WHERE MABAN > 20;
+go
+
+--5.in ra ten khach hang va ten
+DECLARE @temp_CustID int, @temp_name
+varchar(50)
+SET @temp_CustID = 12
+SELECT @temp_name = TENKHACHHANG FROM KHACHHANG
+Where MAKHACHHANG = @temp_CustID
+PRINT 'KhachHangID is'  + @temp_CustID +  'and Name is' +
+@temp_name
+
+--6. cap nhat ten kahch hang co id = 25. neu ko co in ra dong ko dc cap nhat
+Update KHACHHANG set TENKHACHHANG = N'Ronaldo'
+Where MAKHACHHANG =25
+If(@@rowcount=0)
+	begin
+		print N'Không dòng nào được cập nhật'
+	return
+end
+
+--7.in ra cac muc giam gia mon an trong bang mon an
+SELECT MAMON, DONGIA,[discount%]=
+CASE
+	WHEN DONGIA <20000 THEN 10
+	WHEN DONGIA BETWEEN 20000 and 50000 THEN 15
+	WHEN DONGIA BETWEEN 50000 and 1000000 THEN 20
+	ELSE
+		0.0
+	END
+FROM MONAN
+ORDER BY MAMON
+
+--8.tang gap doi don gia cua mon an co gia < 50000
+WHILE (SELECT AVG(DONGIA) FROM MONAN) < 50000
+	BEGIN
+	UPDATE MONAN SET DONGIA = DONGIA * 2
+	IF (SELECT MAX(DONGIA) FROM MONAN) > 50000
+		BREAK
+	ELSE
+		CONTINUE
+END
+
+--9.bien toan cuc
+select @@VERSION,@@SERVERNAME,@@ERROR ;
+go
+
+--10.in ra loai mon cua tung ma mon
+SELECT MAMON,LOAIMON =
+CASE ID_MANHOM
+	WHEN 1 THEN N'tráng miệng'
+	WHEN 2 THEN N'món chính'
+	WHEN 3 THEN N'món phụ'
+	WHEN 4 THEN N'món khai vị'
+	WHEN 5 THEN N'hoa quả'
+	ELSE N'chưa phân nhóm'
+END
+FROM MONAN
+ORDER BY ID_MANHOM
+
+--11.tao thu tuc lay du lieu khach hang
+CREATE PROC KhachHangTimKiem
+@cus_id int = NULL
+AS
+SELECT MAKHACHHANG, TENKHACHHANG, SDT
+FROM KHACHHANG
+WHERE MAKHACHHANG like @cus_id
+RETURN (@@rowcount)
+GO
+
+--12.tao thu tuc dat lai gia moi cho mon an
+create proc MonAn_Update @mamon int,@giamoi float
+as
+update MONAN
+set DONGIA=@giamoi
+where MAMON=@mamon
+go
+
+--thuc thi
+MonAn_Update 2,20000;
+go
+
+--13.lay ra cac khach hang co ma khach hang tuy y
+create function f_SelectCustomer
+(@customerid int)
+returns table
+as
+return (select * from KHACHHANG
+where MAKHACHHANG > @customerid)
+go
+
+--14.xoa 3 nnhan vien dau tien co chua tu 'minh' trong ten
+DELETE TOP(3) 
+FROM NHANVIEN
+WHERE TENNHANVIEN = '%Minh%';
+GO
+
+--15.dung con tro hien tat ca thong tin khach hang
+DECLARE @id int
+DECLARE @name nvarchar(200)
+
+
+DECLARE cursorCustomer CURSOR FOR  
+SELECT MAKHACHHANG, TENKHACHHANG FROM KHACHHANG    
+
+OPEN cursorCustomer                
+
+FETCH NEXT FROM cursorCustomer     
+      INTO @id, @name
+
+WHILE @@FETCH_STATUS = 0          
+BEGIN
+    --In kết quả
+    PRINT 'ID:' + CAST(@id as varchar);
+    PRINT 'Ten khach Hang:' + @name;
+
+    FETCH NEXT FROM cursorCustomer 
+          INTO @id, @name
+END
+
+CLOSE cursorCustomer              
+DEALLOCATE cursorCustomer         
---
+++ T-SQL/Do_Viet_Anh_T-SQL.sql
@@ -0,0 +1,178 @@
+﻿--                       T-SQL                                
+USE new_data;
+GO 
+--1 thêm cột tên khách hàng vào bảng thanh toán 
+ALTER TABLE THANHTOAN ADD TENKHACHHANG nvarchar(50);
+GO
+ALTER TABLE THANHTOAN
+DROP TENKHACHHANG
+--2 xóa nhóm món ăn có mã nhóm =1
+DELETE NHOMMONAN WHERE MANHOM = 1
+GO
+
+--3 Cập nhật tên món, đơn giá của món ăn có mã món  =6
+UPDATE MONAN SET DONGIA = 40000, TENMON= N'lẩu tứ xuyên'
+WHERE MAMON=6
+go
+--4 Tìm bàn của 1 khách hàng có mã bàn = 9 xem thẻ có được nhân viên phục vụ hay không?
+--  Nếu ID_MANHANVIEN = 0 -> bàn ko có nhân viên phục vụ
+--  Nếu ID_MANHANVIEN khác 0 -> bàn có nhân viên phục vụ
+
+IF (SELECT BANAN.ID_MANHANVIEN FROM BANAN WHERE BANAN.MABAN = 9) < 1
+BEGIN
+    PRINT N'bàn ko có nhân viên phục vụ.'
+END
+ELSE
+	BEGIN
+		PRINT N'bàn đã có nhân viên phục vụ.'
+	END
+
+--5 Tìm xem trong bảng khachhang có khách hàng nào có tên là hiếu hay không?
+
+IF EXISTS (SELECT * FROM KHACHHANG WHERE TENKHACHHANG LIKE N'%Hiếu%')
+BEGIN
+    PRINT N'Có khách hàng tên Hiếu:'
+	(SELECT * FROM KHACHHANG WHERE TENKHACHHANG LIKE N'%Hiếu%')
+END
+ELSE
+BEGIN
+	PRINT N'Không có khách hàng nào tên Hiếu'
+END
+
+go
+
+--6 in ra thanh toán của khách hàng và phần giảm giá trong evant của cửa hàng 
+select MAKHACHHANG,KHACHHANG.TENKHACHHANG,MAYEUCAU,TONGTIEN ,[% được giảm]=
+CASE
+	WHEN TONGTIEN <20000 THEN 10
+	WHEN TONGTIEN BETWEEN 20000 and 50000 THEN 15
+	WHEN TONGTIEN BETWEEN 50000 and 1000000 THEN 20
+	ELSE
+		0.0
+	END
+from KHACHHANG,YEUCAU,THANHTOAN
+where KHACHHANG.MAKHACHHANG=YEUCAU.ID_MAKHACHHANG
+      and YEUCAU.MAYEUCAU=THANHTOAN.ID_MAYEUCAU
+	  order by TONGTIEN asc
+GO
+--7 đếm số hóa đơn trong ngày 14
+
+declare @SoHoaDon int
+declare @Ngay datetime
+set @Ngay ='2019-10-14'
+Select @SoHoaDon = Count(MATHANHTOAN) From THANHTOAN where NGAYTT='2019-10-14'
+print N' Số hóa đơn là : '+Convert(Nvarchar(10),@SoHoaDon)
+print N' Ngày = '+Cast(@Ngay as NVarchar(20))
+Go
+
+--8 thông tin món ăn được yêu cầu
+declare @ThongTinMonAn table(
+[tên món] nvarchar(50) not null ,
+[mã món] int not null,
+[số lượng] int not null,
+[trạng thái] nvarchar(50) not null ,
+[thành tiền] int not null
+);
+insert into @ThongTinMonAn
+select
+(select TENMON from MONAN where DONGYEUCAU.ID_MAMON = MONAN.MAMON ),
+(select MAMON from MONAN where DONGYEUCAU.ID_MAMON = MONAN.MAMON),
+(select SOLUONG from DONGYEUCAU where DONGYEUCAU.ID_MAMON = MONAN.MAMON),
+(select TRANGTHAI from DONGYEUCAU where DONGYEUCAU.ID_MAMON = MONAN.MAMON),
+(select THANHTIEN from DONGYEUCAU where DONGYEUCAU.ID_MAMON = MONAN.MAMON)
+from DONGYEUCAU,MONAN
+where DONGYEUCAU.ID_MAMON = MONAN.MAMON
+
+select * 
+from @ThongTinMonAn
+
+--9 cursor  ( truyền giá dựa vào tổng tiền nếu >50000 thì trạng thái là 3
+--										nếu <50000 và >30000 thì trạng thái là 2
+--										còn lại thì 1 
+--           từ đó biết nhân viên nào thanh toán được hóa đơn lớn hơn để tăng lương)
+
+
+DECLARE TANGLUONGCursor CURSOR FOR SELECT TONGTIEN ,ID_MANHANVIEN  FROM THANHTOAN
+
+OPEN TANGLUONGCursor
+
+DECLARE @ID_MANHANVIEN CHAR(100)
+DECLARE @TONGTIEN NVARCHAR(100)
+FETCH NEXT FROM TANGLUONGCursor INTO @ID_MANHANVIEN,@TONGTIEN
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	IF @TONGTIEN > 50000
+	BEGIN
+		PRINT 'ID:' + Cast(@ID_MANHANVIEN as NVarchar(20))
+        PRINT 'TRANG THAI:TOT ' 
+	END
+	ELSE 
+	IF @TONGTIEN < 50000 AND @TONGTIEN > 30000 
+	BEGIN
+		PRINT 'ID:' + Cast(@ID_MANHANVIEN as NVarchar(20))
+        PRINT 'TRANG THAI:KHA '
+	END
+	ELSE
+	BEGIN
+		PRINT 'ID:' + Cast(@ID_MANHANVIEN as NVarchar(20))
+        PRINT 'TRANG THAI:TB '
+	END
+    
+
+	FETCH NEXT FROM TANGLUONGCursor INTO @ID_MANHANVIEN,@TONGTIEN    
+END
+CLOSE TANGLUONGCursor
+DEALLOCATE TANGLUONGCursor
+
+--10 tạo function để xác định trả về tình trạng của bàn ăn có mã bàn là 3   
+
+CREATE FUNCTION trangthaiban(@trangthai int)
+returns nvarchar(50)
+as
+begin
+declare @thongtin nvarchar(50)
+if @trangthai=0 
+set @thongtin = N'bàn chưa được đặt'
+else 
+set @thongtin = N'bàn đã được đặt'
+return @thongtin
+END
+GO
+select MABAN,LOAIBAN,dbo.trangthaiban(TINHTRANG)
+from BANAN
+where MABAN=3
+GO
+--11 trigger
+/* cập nhật hàng trong kho sau khi đặt hàng hoặc cập nhật */
+CREATE TRIGGER trg_DatHang ON DONGYEUCAU AFTER INSERT AS 
+BEGIN
+	UPDATE MONAN
+	SET SoLuongTon = SoLuongTon - (
+		SELECT SoLuongDat
+		FROM inserted
+		WHERE MaHang = tbl_KhoHang.MaHang
+	)
+	FROM tbl_KhoHang
+	JOIN inserted ON tbl_KhoHang.MaHang = inserted.MaHang
+END
+GO
+/* cập nhật hàng trong kho sau khi cập nhật đặt hàng */
+CREATE TRIGGER trg_CapNhatDatHang on tbl_DatHang after update AS
+BEGIN
+   UPDATE tbl_KhoHang SET SoLuongTon = SoLuongTon -
+	   (SELECT SoLuongDat FROM inserted WHERE MaHang = tbl_KhoHang.MaHang) +
+	   (SELECT SoLuongDat FROM deleted WHERE MaHang = tbl_KhoHang.MaHang)
+   FROM tbl_KhoHang 
+   JOIN deleted ON tbl_KhoHang.MaHang = deleted.MaHang
+end
+GO
+/* cập nhật hàng trong kho sau khi hủy đặt hàng */
+create TRIGGER trg_HuyDatHang ON tbl_DatHang FOR DELETE AS 
+BEGIN
+	UPDATE tbl_KhoHang
+	SET SoLuongTon = SoLuongTon + (SELECT SoLuongDat FROM deleted WHERE MaHang = tbl_KhoHang.MaHang)
+	FROM tbl_KhoHang 
+	JOIN deleted ON tbl_KhoHang.MaHang = deleted.MaHang
+END
+
+--12 Transaction
